b748a490d1bbad7eef813d12e21c6c26fbe2ef93
KeshavSPandya <keshav20pandya@gmail.com>
2025-09-06 20:22:36 +0530

    Ensure buffer update before sweep transfer

--- contracts/src/interfaces/IPSM.sol
+++ contracts/src/interfaces/IPSM.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.24;
 interface IPSM {
     function swapStableFor0xUSD(address stable, uint256 amount, uint256 minOut) external;
     function swap0xUSDForStable(address stable, uint256 amount, uint256 minOut) external;
+    function sweep(address stable, address to, uint256 amt) external;
 }
--- contracts/test/PSM.t.sol
+++ contracts/test/PSM.t.sol
@@ -5,26 +5,41 @@ import {Test} from "forge-std/Test.sol";
 import {PSM} from "../src/psm/PSM.sol";
 import {OxUSD} from "../src/token/0xUSD.sol";
 
+contract MockStable {
+    function transfer(address, uint256) external returns (bool) {
+        return true;
+    }
+}
+
 contract PSMTest is Test {
     PSM psm;
     OxUSD token;
-    address stable = address(0xdead);
+    MockStable stable;
 
     function setUp() public {
         token = new OxUSD();
         psm = new PSM(token, address(this));
         token.setMinters(address(psm), address(0));
-        psm.setRoute(stable, 0, type(uint256).max);
+        stable = new MockStable();
+        psm.setRoute(address(stable), 0, type(uint256).max);
     }
 
     function testSwapStableFor0xUSD() public {
-        psm.swapStableFor0xUSD(stable, 100, 99);
+        psm.swapStableFor0xUSD(address(stable), 100, 99);
         assertEq(token.balanceOf(address(this)), 100);
     }
 
     function testHaltReverts() public {
-        psm.halt(stable, true);
+        psm.halt(address(stable), true);
         vm.expectRevert();
-        psm.swapStableFor0xUSD(stable, 1, 0);
+        psm.swapStableFor0xUSD(address(stable), 1, 0);
+    }
+
+    function testSweepDecreasesBuffer() public {
+        psm.swapStableFor0xUSD(address(stable), 100, 99);
+        (uint256 beforeBuffer, , , ) = psm.routes(address(stable));
+        psm.sweep(address(stable), address(this), 40);
+        (uint256 afterBuffer, , , ) = psm.routes(address(stable));
+        assertEq(afterBuffer, beforeBuffer - 40);
     }
 }
