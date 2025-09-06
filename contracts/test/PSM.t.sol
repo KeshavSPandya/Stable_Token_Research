1b1f9b35ae88e3e6ae8811a11769e60c8cf8951f
KeshavSPandya <keshav20pandya@gmail.com>
2025-09-06 20:42:54 +0530

    test: add swap0xUSDForStable tests

--- contracts/test/PSM.t.sol
+++ contracts/test/PSM.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {Test} from "forge-std/Test.sol";
 import {PSM} from "../src/psm/PSM.sol";
 import {OxUSD} from "../src/token/0xUSD.sol";
+import {DepthExceeded, RouteHalted, StaleParity} from "../src/libs/Errors.sol";
 
 contract PSMTest is Test {
     PSM psm;
@@ -26,5 +27,33 @@ contract PSMTest is Test {
         psm.halt(stable, true);
         vm.expectRevert();
         psm.swapStableFor0xUSD(stable, 1, 0);
+    }
+
+    function testSwap0xUSDForStable() public {
+        psm.swapStableFor0xUSD(stable, 100, 0);
+        token.approve(address(psm), 100);
+        psm.swap0xUSDForStable(stable, 50, 49);
+        assertEq(token.balanceOf(address(this)), 50);
+        (uint256 buffer,,,) = psm.routes(stable);
+        assertEq(buffer, 50);
+    }
+
+    function testDepthExceededReverts() public {
+        psm.swapStableFor0xUSD(stable, 100, 0);
+        vm.expectRevert(DepthExceeded.selector);
+        psm.swap0xUSDForStable(stable, 101, 0);
+    }
+
+    function testRouteHaltedReverts() public {
+        psm.halt(stable, true);
+        vm.expectRevert(RouteHalted.selector);
+        psm.swap0xUSDForStable(stable, 1, 0);
+    }
+
+    function testSlippageReverts() public {
+        psm.swapStableFor0xUSD(stable, 100, 0);
+        token.approve(address(psm), 100);
+        vm.expectRevert(StaleParity.selector);
+        psm.swap0xUSDForStable(stable, 50, 51);
     }
 }
