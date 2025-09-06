4026671e956e4bbbef20f1524545a75954e81f1b
KeshavSPandya <keshav20pandya@gmail.com>
2025-09-06 20:41:19 +0530

    Add ParamRegistry address setting tests

--- contracts/src/governance/ParamRegistry.sol
+++ contracts/src/governance/ParamRegistry.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {NotAuthorized} from "../libs/Errors.sol";
+import {NotAuthorized, ZeroAddress} from "../libs/Errors.sol";
 
 contract ParamRegistry {
     address public guardian;
     address public timelock;
     address public dao;
+    mapping(bytes32 => address) public addrs;
 
     event SpreadSet(address stable, uint256 bps);
+    event AddrSet(bytes32 indexed key, address indexed value);
 
     constructor(address _guardian, address _timelock, address _dao) {
         guardian = _guardian;
@@ -23,5 +25,11 @@ contract ParamRegistry {
 
     function setSpread(address stable, uint256 bps) external onlyDao {
         emit SpreadSet(stable, bps);
+    }
+
+    function setAddr(bytes32 key, address value) external onlyDao {
+        if (value == address(0)) revert ZeroAddress();
+        addrs[key] = value;
+        emit AddrSet(key, value);
     }
 }
